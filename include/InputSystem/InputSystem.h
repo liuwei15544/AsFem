4a2ed4d2ae13ea6fbf6ddc87055f59afc719fb52
walkandthinker <walkandthinker@gmail.com>
2018-08-18 20:49:08 +0200

    add remove space

---
+++ include/InputSystem/InputSystem.h
@@ -0,0 +1,24 @@
+//*****************************************************
+//*** AsFem: a simple finite element method program ***
+//*** Copyright (C) 2018 walkandthinker             ***
+//*** Contact: walkandthinker@gmail.com             ***
+//*****************************************************
+//* This file is part of the ASFEM framework
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* Licensed under GPL 3.0, please see LICENSE for details
+//******************************************************
+//
+// Created by walkandthinker on 16.08.18.
+// Define the input file read system in AsFem
+
+#ifndef ASFEM_INPUTSYSTEM_H
+#define ASFEM_INPUTSYSTEM_H
+
+
+class InputSystem
+{
+
+};
+
+
+#endif //ASFEM_INPUTSYSTEM_H
