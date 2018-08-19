246441ac0e30cc9dab2aa7990086907073a570d3
walkandthinker <walkandthinker@gmail.com>
2018-08-19 20:50:03 +0200

    add gauss point

---
+++ include/GaussPoint/GaussPoint.h
@@ -0,0 +1,21 @@
+//*****************************************************
+//*** AsFem: a simple finite element method program ***
+//*** Copyright (C) 2018 walkandthinker             ***
+//*** Contact: walkandthinker@gmail.com             ***
+//*****************************************************
+//* This file is part of the ASFEM framework
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* Licensed under GPL 3.0, please see LICENSE for details
+//******************************************************
+//
+// Created by walkandthinker on 19.08.18.
+// Define gauss integration points and weights in AsFem
+
+#ifndef ASFEM_GAUSSPOINT_H
+#define ASFEM_GAUSSPOINT_H
+
+void Int1D(int ngp,int &Lint,double (&gs)[125][4]);
+void Int2D(int ngp,int &Lint,double (&gs)[125][4]);
+void Int3D(int ngp,int &Lint,double (&gs)[125][4]);
+
+#endif //ASFEM_GAUSSPOINT_H
