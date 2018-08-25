ca9c072d885c31d4211ece1220af8eeaad0f435b
walkandthinker <walkandthinker@gmail.com>
2018-08-25 19:50:47 +0200

    add ICSystem header file

---
+++ include/ICSystem/ICSystem.h
@@ -0,0 +1,24 @@
+//*****************************************************
+//*** AsFem: a simple finite element method program ***
+//*** Copyright (C) 2018 walkandthinker             ***
+//*** Contact: walkandthinker@gmail.com             ***
+//*****************************************************
+//* This file is part of the ASFEM framework
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* Licensed under GPL 3.0, please see LICENSE for details
+//******************************************************
+//
+// Created by walkandthinker on 18.08.18.
+// define the initial condition system in AsFem
+
+#ifndef ASFEM_ICSYSTEM_H
+#define ASFEM_ICSYSTEM_H
+
+
+class ICSystem
+{
+
+};
+
+
+#endif //ASFEM_ICSYSTEM_H
