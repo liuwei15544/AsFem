043a399ce60f16d8bf7815509fa0086240f4f45c
walkandthinker <walkandthinker@gmail.com>
2019-04-01 17:49:36 +0200

    first test

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "Eigen/Eigen"
+
+using namespace std;
+using namespace Eigen;
+
+int main(int args,char *argv[])
+{
+    cout<<"Hello eigen!"<<endl;
+    return 0;
+}
