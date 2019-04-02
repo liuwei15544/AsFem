58240d5d7ec2f1bcc90b2adc990e1736d37b4abc
walkandthinker <walkandthinker@gmail.com>
2019-04-02 21:58:12 +0200

    add Welcome

---
+++ include/Welcome.h
@@ -0,0 +1,22 @@
+#ifndef ASFEM_WELCOME_H
+#define ASFEM_WELCOME_H
+
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+
+using namespace std;
+
+void Welcome(double version=0.1)
+{
+    cout<<"****************************************************"<<endl;
+    cout<<"*** Welcome to use AsFem                         ***"<<endl;
+    cout<<"*** A Simple Finite Element Method program       ***"<<endl;
+    printf("*** Version: %5.1f                               ***\n",version);
+    cout<<"*** Author : walkandthinker                      ***"<<endl;
+    cout<<"*** Contact: walkandthinker@gmail.com            ***"<<endl;
+    cout<<"****************************************************"<<endl;
+}
+
+#endif 
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 #include "Eigen/Eigen"
+#include "Welcome.h"
 
 using namespace std;
 using namespace Eigen;
 
 int main(int args,char *argv[])
 {
+    Welcome(1.2);
     return 0;
 }
